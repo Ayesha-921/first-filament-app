5ab65315c4be3cded35db18220000bffd185b811
Ayesha_code_hub <ayesha921khan@gmail.com>
2026-05-01 01:16:18 +0500

    reset nav-bar

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -18,7 +18,7 @@
     {{-- Nav --}}
     <nav class="bg-gray-900 text-white px-4 py-2 flex items-center gap-4">
         <a href="{{ route('products.index') }}" class="text-xl font-bold text-white hover:text-yellow-400">
-            🛒 YourShop
+            🛒 MyShop
         </a>
         <div class="flex-1 flex">
             <input type="text" placeholder="Search products..."
